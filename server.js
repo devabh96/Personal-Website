5f7e9b337bbe0366f00c5e6c5d4750bbde87b7d8
Devabh Chandra <devabhchandra7@gmail.com>
2024-11-02 22:24:03 +0530

    .

---
+++ server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const fastify = require('fastify')({
+  logger: true
+});
+const fastifyStatic = require('@fastify/static');
+const path = require('path');
+const fs = require('fs');
+const routesPath = path.join(__dirname, 'routes');
+
+
+fs.readdirSync(routesPath).forEach((file) => {
+  const route = require(path.join(routesPath, file));
+  fastify.register(route);
+});
+
+fastify.register(fastifyStatic, {
+  root: path.join(__dirname, 'public'),
+  prefix: '/',
+});
+
+
+
+
+
+
+fastify.listen({ port: process.env.PORT }, (err, address) => {
+  if (err) throw err
+})
