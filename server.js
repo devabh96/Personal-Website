dce24dc4d7b9a27d219ac4acdf560719513bdf3d
Devabh Chandra <devabhchandra7@gmail.com>
2024-11-04 15:27:41 +0530

    Some formatting and some discord logging

--- server.js
+++ server.js
@@ -1,44 +1,73 @@
-require('dotenv').config();
-const fastify = require('fastify')({
-  logger: true
+require("dotenv").config();
+const fastify = require("fastify")({
+    logger: true
 });
-const fastifyStatic = require('@fastify/static');
-const path = require('path');
-const fs = require('fs');
-const routesPath = path.join(__dirname, 'routes');
-const mongoose = require('mongoose');
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const fastifyStatic = require("@fastify/static");
+const path = require("path");
+const fs = require("fs");
+const routesPath = path.join(__dirname, "routes");
+const mongoose = require("mongoose");
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
 
-fs.readdirSync(routesPath).forEach((file) => {
-  const route = require(path.join(routesPath, file));
-  fastify.register(route);
+client.login(process.env.BOT_TOKEN);
+client.channels.cache.get(process.env.CHANNEL);
+fs.readdirSync(routesPath).forEach(file => {
+    const route = require(path.join(routesPath, file));
+    fastify.register(route);
 });
 
 fastify.register(fastifyStatic, {
-  root: path.join(__dirname, 'public'),
-  prefix: '/',
+    root: path.join(__dirname, "public"),
+    prefix: "/"
 });
 
 fastify.setNotFoundHandler((request, reply) => {
-  reply.status(404).sendFile('/html/404.html', path.join(__dirname, 'public'));
+    reply
+        .status(404)
+        .sendFile("/html/404.html", path.join(__dirname, "public"));
 });
 
-
-
-
-
-
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+async function mongooseConnect() {
+    await mongoose
+        .connect(process.env.MONGO_URI)
+        .then(() => {
+            client.channels.cache.get(process.env.CHANNEL).send({
+                content: "Connected to the database sucessfully"
+            });
+            console.log("Successfully connected to the database");
+        })
+        .catch(err => {
+            console.error("Error while connecting", err);
+        });
+}
+async function fastifyConnect() {
+    await fastify.listen(
+        {
+            port: process.env.PORT,
+            host: process.env.HOST
+        },
+        (err, address) => {
+            if (err)
+                client.channels.cache.get(process.env.CHANNEL).send({
+                    content: err
+                });
+                else client.channels.cache.get(process.env.CHANNEL).send({
+                content: "Fastify is up"})
+        }
+    );
+}
+client.once("ready", async () => {
+    client.channels.cache.get(process.env.CHANNEL).send({
+        content: "Bot is ready"
+    });
+    console.log(`Logged in as ${client.user.tag}!`);
+    await mongooseConnect();
+    await fastifyConnect();
 });
-client.login(process.env.BOT_TOKEN);
-mongoose.connect(process.env.MONGO_URI).then(()=>{
-  console.log("Successfully connected to the database")
-}).catch((err) =>{
-  console.error("Error while connecting",err)
-})
-fastify.listen({ port: process.env.PORT, host: process.env.HOST }, (err, address) => {
-  if (err) throw err
-})
